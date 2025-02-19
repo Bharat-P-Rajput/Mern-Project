98e2102eaf50ff71a4faf1a8274d7609ca2b82e2
Bharat Rajput <bharat.premsing.rajpoot@gmail.com>
2025-02-19 22:13:35 +0530

    Added Routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("Hellow World");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use("/api/goals", require("./routes/goalRouts"));
+
+app.listen(port, () => console.log(`Server Started on port ${port}`));
