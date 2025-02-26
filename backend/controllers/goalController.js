4b393f3c50015a94d67181e9fe84f1ebccfc63d6
Bharat Rajput <bharat.premsing.rajpoot@gmail.com>
2025-02-26 22:53:25 +0530

    User Registraion and User Login API

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -52,7 +52,7 @@ const deleteGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Goal not Found");
   }
-  await goal.remove();
+  await goal.deleteOne();
   res.status(200).json({ id: req.params.id });
 });
 
--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", require("./routes/goalRouts"));
+app.use("/api/users", require("./routes/userRouts"));
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server Started on port ${port}`));
